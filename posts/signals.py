889c40deba128d94f02244cae277e03ad3d64acf
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-02-08 15:06:26 +0500

    commit

---
+++ posts/signals.py
@@ -0,0 +1,16 @@
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
+
+
+from posts.models import PostModel
+
+
+@receiver(pre_save, sender=PostModel)
+def generate_slag_for_post(sender, instance, **kwargs):
+    original_blog = slugify(instance.title)
+    slug = original_blog
+    count = 0
+    while PostModel.objects.filter(slug=slug).exists():
+        slug = f"{original_blog}-{count}"
+
