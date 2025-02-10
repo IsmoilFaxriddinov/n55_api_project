83a6d33c13ca74bfe716da339c6ae548789b6b4c
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-02-10 20:54:24 +0500

    commit

--- posts/models.py
+++ posts/models.py
@@ -3,11 +3,13 @@ from django.contrib.auth.models import User
 
 class PostModel(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    slug = models.SlugField(unique=True, null=True)
     title = models.CharField(max_length=125)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def save(self, **kwargs):
+        slug = 
         return super().save(**kwargs)
 
     def __str__(self):
