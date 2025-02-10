1c1ac03268972699e0562c9c84de3cc962c23461
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-02-10 21:23:07 +0500

    commit

--- posts/signals.py
+++ posts/signals.py
@@ -8,12 +8,13 @@ from posts.models import PostModel
 
 @receiver(pre_save, sender=PostModel)
 def generate_slag_for_post(sender, instance, **kwargs):
-    original_blog = slugify(instance.title)
-    slug = original_blog
-    count = 0
-    while PostModel.objects.filter(slug=slug).exists():
-        slug = f"{original_blog}-{count}"
-        count += 1
+    if not instance.slug:   
+        original_blog = slugify(instance.title)
+        slug = original_blog
+        count = 0
+        while PostModel.objects.filter(slug=slug).exists():
+            slug = f"{original_blog}-{count}"
+            count += 1
 
     instance.slug = slug
 
