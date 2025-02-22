0398776d05e093f7bf252445ddb8b06ee82bb36f
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-02-22 20:59:16 +0500

    commit

--- posts/urls.py
+++ posts/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from posts.views import PostAPIView, PostDetailAPIView
+from posts.views import PersonalListView, PostAPIView, PostDetailAPIView, PostRetrieveUpdateDestroyAPIView
 
 app_name = 'posts'
 
 urlpatterns = [
     path('', PostAPIView.as_view(), name='list'),
-    path("<slug:slug>/", PostDetailAPIView.as_view(), name="detail"),
-    path("me/", PostDetailAPIView.as_view(), name="my-posts"),
+    path("me/", PersonalListView.as_view(), name="my-posts"),
+    path("<slug:slug>/", PostRetrieveUpdateDestroyAPIView.as_view(), name="detail"),
     path("<slug:slug>/claps/", PostDetailAPIView.as_view(), name="claps"),
     path("<slug:slug>/comments/", PostDetailAPIView.as_view(), name="claps"),
     path("<slug:slug>/comments/claps/", PostDetailAPIView.as_view(), name="claps"),
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,14 @@
+from urllib import request
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListAPIView
 
 from app_common.paginations import StandardResultsSetPagination
+from app_common.permissions import IsOwnerOrReadOnly
 from posts.models import PostModel
 from posts.serializers import PostsSerializers
 
@@ -35,55 +38,42 @@ class PostAPIView(APIView):
 
 class PostDetailAPIView(APIView):
     serializer_class = PostsSerializers
+    permission_classes = [IsOwnerOrReadOnly]
 
     def get(self, request, slug):
         post = self.get_object(slug=slug)
+        self.check_object_permissions(request, post)
         serializer = self.serializer_class(post)
-        return Response({
-            "success": True,
-            "data": serializer.data
-        }, status=status.HTTP_200_OK)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, slug):
         post = self.get_object(slug=slug)
+        self.check_object_permissions(request, post)
         serializer = PostsSerializers(post, data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return Response(data={
-                "success": True,
-                "data": serializer.data
-            }, status=status.HTTP_202_ACCEPTED)
+            return Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
 
-        return Response(data={
-            "success": False,
-            "detail": serializer.errors
-        }, status=status.HTTP_400_BAD_REQUEST)
+        return Response(data=serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
     def patch(self, request, slug):
         post = self.get_object(slug=slug)
+        self.check_object_permissions(request, post)
         serializer = PostsSerializers(post, data=request.data, partial=True)
 
         if serializer.is_valid():
             serializer.save()
-            return Response(data={
-                "success": True,
-                "data": serializer.data
-            }, status=status.HTTP_202_ACCEPTED)
+            return Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
     
 
-        return Response(data={
-            "success": False,
-            "detail": serializer.errors
-        }, status=status.HTTP_400_BAD_REQUEST)
+        return Response(data=serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, slug):
         post = self.get_object(slug=slug)
+        self.check_object_permissions(request, post)
         post.delete()
-        return Response(data={
-            "success": True,
-            "detail": "Post is deleted"
-        }, status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     @staticmethod
     def get_object(slug):
@@ -95,3 +85,18 @@ class PostDetailAPIView(APIView):
                 "detail": "Post does not found"
             })
 
+class PostRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = PostsSerializers
+    queryset = PostModel.objects.all()
+    lookup_field = 'slug'
+    permission_classes = [IsOwnerOrReadOnly]
+    
+
+class PersonalListView(ListAPIView):
+    serializer_class = PostsSerializers
+    pagination_class = StandardResultsSetPagination
+    permission_classes = [IsAuthenticated]
+    # queryset = PostModel.objects.all()
+
+    def get_queryset(self):
+        return PostModel.objects.filter(author=self.request.user)
